79d22e1901aea12a565497d1ada3ebc1fe5feda0
Alex Grover <hello@alexgrover.me>
2023-10-29 18:47:32 -0700

    Add noggles to header

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -2,12 +2,14 @@
 
 import { ConnectKitButton } from 'connectkit'
 import Link from 'next/link'
+import Noggles from '@/components/noggles'
 import styles from './header.module.css'
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <Link href="/">
+      <Link href="/" className={styles.link}>
+        <Noggles className={styles.logo} />
         <h1>nouns.zone</h1>
       </Link>
       <ConnectKitButton />
