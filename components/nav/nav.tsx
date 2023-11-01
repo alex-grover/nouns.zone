39ca3227d86717a68c1e514cac4645cad494e970
Alex Grover <hello@alexgrover.me>
2023-10-31 19:59:28 -0700

    Update sidebar styling

--- components/nav/nav.tsx
+++ components/nav/nav.tsx
@@ -1,29 +1,56 @@
-import { ActivityIcon, HelpCircleIcon, TextIcon } from 'lucide-react'
+'use client'
+
+import {
+  ActivityIcon,
+  ExternalLinkIcon,
+  HelpCircleIcon,
+  TextIcon,
+} from 'lucide-react'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import styles from './nav.module.css'
 
-// TODO: background + bold for current route, hover
 export default function Nav() {
+  const pathname = usePathname()
+
   return (
     <nav className={styles.nav}>
       <ol className={styles.list}>
         <li>
-          <Link href="/" className={styles.link}>
+          <Link
+            href="/"
+            className={styles.link}
+            data-selected={pathname === '/'}
+          >
             <TextIcon />
             <span>Discussion</span>
           </Link>
         </li>
         <li>
-          <Link href="/activity" className={styles.link}>
+          <Link
+            href="/activity"
+            className={styles.link}
+            data-selected={pathname === '/activity'}
+          >
             <ActivityIcon />
             <span>Activity</span>
           </Link>
         </li>
         <li>
-          <Link href="/about" className={styles.link}>
+          <Link
+            href="/about"
+            className={styles.link}
+            data-selected={pathname === '/about'}
+          >
             <HelpCircleIcon />
             <span>About</span>
           </Link>
+        </li>
+        <li>
+          <a href="https://nouns.wtf" className={styles.link}>
+            <ExternalLinkIcon />
+            <span>Nouns DAO</span>
+          </a>
         </li>
       </ol>
     </nav>
