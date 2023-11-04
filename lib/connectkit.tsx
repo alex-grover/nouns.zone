08b6fb0eec601b9c9cab89f4b4a8826038f625ed
Alex Grover <hello@alexgrover.me>
2023-11-04 11:21:02 -0700

    Specify wagmi chains

--- lib/connectkit.tsx
+++ lib/connectkit.tsx
@@ -2,11 +2,12 @@
 
 import { ConnectKitProvider, getDefaultConfig } from 'connectkit'
 import { PropsWithChildren } from 'react'
-import { createConfig, WagmiConfig } from 'wagmi'
+import { createConfig, mainnet, WagmiConfig } from 'wagmi'
 import env from '@/lib/env'
 
 const config = createConfig(
   getDefaultConfig({
+    chains: [mainnet],
     walletConnectProjectId: env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
     appName: 'nouns.zone',
     appDescription: 'All things Nouns on Farcaster',
